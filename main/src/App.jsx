4e24fc7be9a79ccfd08bf42a37eec09af9e9be7b
ivan <107969090+free-hornet@users.noreply.github.com>
2024-05-10 21:33:04 -0400

    part1 courseinfo complete

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -36,7 +36,6 @@ const App = () => {
       <Header course={course}/>
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
